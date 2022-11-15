539fbc0695a1941e7c0c1e5a7571b348c16364e6
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-11-15 11:12:32 -0500

    Set up link to movie detail page from movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,12 +4,15 @@ import './MovieCard.css'
 export default function MovieCard({index, movie}) {
     console.log('moviecard',index, movie)
     return(
-        <div className='MovieCard card'>
-            <img className='card-img-top' src={`${movie.posterPath}`} alt="Image not found" />
-            <div className='card-body'>
-                <h5 className='title card-title'>{movie.title}</h5>
-                <h6 className='card-text'>{movie.releaseDate}</h6>
+        <Link to={`/movies/:${movie.title}`}>
+            <div className='MovieCard card'>
+                <img className='card-img-top' src={`${movie.posterPath}`} alt="Image not found" />
+                <div className='card-body'>
+                    <h5 className='title card-title'>{movie.title}</h5>
+                    <h6 className='card-text'>{movie.releaseDate}</h6>
+                </div>
             </div>
-        </div>
+        </Link>
+        
     )
 }
